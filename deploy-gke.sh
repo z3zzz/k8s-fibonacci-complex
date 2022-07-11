929daf29cd6bcb29561ecefe0734ff47ba1e6a9f
z3zzz <zabra7946@naver.com>
2022-07-11 14:59:16 +0900

    Update deply gke sh

--- deploy-gke.sh
+++ deploy-gke.sh
@@ -2,6 +2,9 @@ curl https://sdk.cloud.google.com > install.sh;
 bash install.sh --disable-prompts;
 source $HOME/google-cloud-sdk/path.bash.inc;
 
+# decrypt service-account.json for google cloud sdk
+openssl aes-256-cbc -K $encrypted_9f3b5599b056_key -iv $encrypted_9f3b5599b056_iv -in service-account.json.enc -out service-account.json -d
+
 gcloud components update kubectl --quiet;
 gcloud auth activate-service-account --key-file service-account.json;
 gcloud config set project kube8-practice;
